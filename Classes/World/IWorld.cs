fe907b505136cdc2d498145f2d0e4a0ad62866eb
Ignas Sadonis <ignassadonis@gmail.com>
2019-09-02 21:16:22 +0100

    Added more functions to IWorld

--- Classes/World/IWorld.cs
+++ Classes/World/IWorld.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using OQ.MineBot.GUI.Protocol.Movement.Maps;
+using OQ.MineBot.PluginBase.Classes.Blocks;
 using OQ.MineBot.PluginBase.Classes.Entity.Lists;
 using OQ.MineBot.PluginBase.Classes.Physics.Colliders;
 using OQ.MineBot.PluginBase.Pathfinding;
@@ -49,6 +52,33 @@ namespace OQ.MineBot.PluginBase.Classes.World
         byte GetBlockMetadata(int x, int y, int z);
 
         IBlock GetLookingAt(); // may be null.
+
+        Task<IEnumerable<IBlock>> GetPathableBlocksFrom(ILocation location, uint range, MapOptions mapOptions = null);
+        Task<IEnumerable<IBlock>> GetPathableBlocksFrom(IPosition position, uint range, MapOptions mapOptions = null);
+        Task<IEnumerable<IBlock>> GetPathableBlocksFrom(int x, int y, int z, uint range, MapOptions mapOptions = null);
+
+        Task<bool> PlaceAt(ILocation location);
+        Task<bool> PlaceAt(IPosition position);
+        Task<bool> PlaceAt(int x, int y, int z);
+        Task<bool> PlaceOn(FaceData faceData);
+        Task<bool> PlaceOn(ILocation location,  sbyte face);
+        Task<bool> PlaceOn(IPosition position,  sbyte face);
+        Task<bool> PlaceOn(int x, int y, int z, sbyte face);
+
+        Task<IDigAction> Dig(FaceData fadeData);
+        Task<IDigAction> Dig(ILocation location, sbyte face);
+        Task<IDigAction> Dig(ILocation location);
+        Task<IDigAction> Dig(IPosition position);
+        Task<IDigAction> Dig(int x, int y, int z);
+
+        Task<IBlock[]> FindFrom(ILocation location, int width, int height, ushort id, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock[]> FindFrom(ILocation location, int width, int height, ushort[] ids, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock> FindClosestTo(ILocation location, int width, int height, ushort id, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock> FindClosestTo(ILocation location, int width, int height, ushort[] ids, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock[]> Find(int width, int height, ushort id, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock[]> Find(int width, int height, ushort[] ids, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock> FindClosest(int width, int height, ushort id, Func<IBlock, bool> optionalIsPickable = null);
+        Task<IBlock> FindClosest(int width, int height, ushort[] ids, Func<IBlock, bool> optionalIsPickable = null);
 
         bool IsWalkable(ILocation location); // Internal legacy requirement, mark as _IsWalkable
     }
